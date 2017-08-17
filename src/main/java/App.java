d3b0945767230c23bfafdcb1cc60c5615d5d80ee
dennismuturia <dmuturia2922@gmail.com>
2017-08-17 07:19:53 +0300

    Another addition

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,39 +1,47 @@
-import java.util.Map;
 import java.util.HashMap;
-
+import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
-import java.util.ArrayList;
 
-public class App{
-  public static void main(String []args){
+public class App {
+  public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("task",request.session().attribute("task"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/tasks", (request, response) -> {
-    Map<String, Object> model = new HashMap<String, Object>();
+    get("/tasks/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Task task = Task.find(Integer.parseInt(request.params(":id")));
+      model.put("task", task);
+      model.put("template", "templates/task.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+    
+    get("tasks/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/task-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
-    ArrayList<Task> tasks = request.session().attribute("tasks");
-    if (tasks == null) {
-      tasks = new ArrayList<Task>();
-      request.session().attribute("tasks", tasks);
-    }
+    get("/tasks", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("tasks", Task.all());
+      model.put("template", "templates/tasks.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
-    String description = request.queryParams("description");
-    Task newTask = new Task(description);
-    tasks.add(newTask);
-
-    model.put("template", "templates/success.vtl");
-    return new ModelAndView(model, layout);
-   }, new VelocityTemplateEngine());
-
+    post("/tasks", (request,response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String description = request.queryParams("description");
+      Task newTask = new Task(description);
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class TaskTest{
 
@@ -12,5 +13,44 @@ public class TaskTest{
   public void Task_instantiatesWithDescription_String() {
     Task myTask = new Task("Mow the lawn");
     assertEquals("Mow the lawn", myTask.getDescription());
+  }
+
+  @Test
+  public void IsCompleted_afterInstatiation_false(){
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(false, myTask.isCompleted());
+  }
+
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
+    }
+
+  @Test
+  public void all_returnsAllInstances_True(){
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Buy me a car");
+    assertEquals(true, Task.all().contains(firstTask));
+    assertEquals(true, Task.all().contains(secondTask));
+  }
+  @Test
+  public void clear_emptiesAllTasksFromArrayList_0() {
+    Task myTask = new Task("Mow the lawn");
+    Task.clear();
+    assertEquals(Task.all().size(), 0);
+  }
+
+  @Test
+  public void instantiatesWithAnId(){
+    Task.clear();
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(1, myTask.getId());
+  }
+  @Test
+  public void find_returnsTaskWithSameId_secondTask() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Buy groceries");
+    assertEquals(Task.find(secondTask.getId()), secondTask);
   }
 }
